9e2f57ed8ea0dcba691d2dd514320bef1cb4348d
EldoDebug <75922660+EldoDebug@users.noreply.github.com>
2025-01-05 14:41:26 +0900

    Revert "Update Cleanable.java"
    This reverts commit 12121c3c73a629b940d928dc76515051442a3ea5.

--- shared/java/impl/Cleanable.java
+++ shared/java/impl/Cleanable.java
@@ -1,25 +1,46 @@
 package io.github.humbleui.skija.impl;
 
-import java.lang.ref.Cleaner;
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
 import java.util.Objects;
 
 public final class Cleanable {
 
-    private static final Cleaner cleaner = Cleaner.create();
+    private static final MethodHandle createCleanerHandle;
+    private static final MethodHandle cleanHandle;
+
+    static {
+        try {
+            Class<?> cleanerClass = Class.forName("sun.misc.Cleaner");
+            createCleanerHandle = MethodHandles.publicLookup().findStatic(cleanerClass, "create", MethodType.methodType(cleanerClass, Object.class, Runnable.class));
+            cleanHandle = MethodHandles.publicLookup().findVirtual(cleanerClass, "clean", MethodType.methodType(void.class));
+        } catch (Throwable e) {
+            throw new NoClassDefFoundError("sun.misc.Cleaner");
+        }
+    }
 
     public static Cleanable register(Object obj, Runnable action) {
         Objects.requireNonNull(obj);
         Objects.requireNonNull(action);
-        return new Cleanable(cleaner.register(obj, action));
+        try {
+            return new Cleanable(createCleanerHandle.invokeWithArguments(obj, action));
+        } catch (Throwable e) {
+            throw new RuntimeException("Unreachable", e);
+        }
     }
 
-    private final Cleaner.Cleanable cleanable;
+    private final Object cleaner;
 
-    private Cleanable(Cleaner.Cleanable cleanable) {
-        this.cleanable = cleanable;
+    private Cleanable(Object cleaner) {
+        this.cleaner = cleaner;
     }
 
     public void clean() {
-        cleanable.clean();
+        try {
+            cleanHandle.invokeWithArguments(cleaner);
+        } catch (Throwable e) {
+            throw new RuntimeException("Unreachable", e);
+        }
     }
 }
