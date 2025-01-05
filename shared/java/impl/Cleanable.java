4d11e829fbe02e1a29d45238e5a38fee2138fb6a
EldoDebug <75922660+EldoDebug@users.noreply.github.com>
2025-01-05 14:57:07 +0900

    Update Cleanable.java

--- shared/java/impl/Cleanable.java
+++ shared/java/impl/Cleanable.java
@@ -1,116 +1,53 @@
 package io.github.humbleui.skija.impl;
 
+import java.lang.ref.PhantomReference;
+import java.lang.ref.ReferenceQueue;
 import java.util.Objects;
-import java.lang.invoke.MethodHandle;
-import java.lang.invoke.MethodHandles;
-import java.lang.invoke.MethodType;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public final class Cleanable {
 
-    private static final CleanerWrapper cleanerWrapper;
+    private static final ReferenceQueue<Object> referenceQueue = new ReferenceQueue<>();
+    private static final ConcurrentHashMap<PhantomReference<?>, Runnable> cleanupActions = new ConcurrentHashMap<>();
+    private static final ExecutorService cleanerExecutor = Executors.newSingleThreadExecutor();
 
     static {
-        CleanerWrapper temp = null;
-        try {
-            temp = new JavaRefCleanerWrapper();
-        } catch (Throwable e) {
+        cleanerExecutor.execute(() -> {
             try {
-                temp = new SunMiscCleanerWrapper();
-            } catch (Throwable ex) {
-                throw new NoClassDefFoundError("No suitable Cleaner implementation found");
+                while (true) {
+                    PhantomReference<?> ref = (PhantomReference<?>) referenceQueue.remove();
+                    Runnable action = cleanupActions.remove(ref);
+                    if (action != null) {
+                        action.run();
+                    }
+                }
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
             }
-        }
-        cleanerWrapper = temp;
+        });
     }
 
     public static Cleanable register(Object obj, Runnable action) {
         Objects.requireNonNull(obj);
         Objects.requireNonNull(action);
-        return new Cleanable(cleanerWrapper.register(obj, action));
+        PhantomReference<Object> ref = new PhantomReference<>(obj, referenceQueue);
+        cleanupActions.put(ref, action);
+        return new Cleanable(ref);
     }
 
-    private final Object cleanerInstance;
+    private final PhantomReference<?> reference;
 
-    private Cleanable(Object cleanerInstance) {
-        this.cleanerInstance = cleanerInstance;
+    private Cleanable(PhantomReference<?> reference) {
+        this.reference = reference;
     }
 
     public void clean() {
-        cleanerWrapper.clean(cleanerInstance);
-    }
-
-    private interface CleanerWrapper {
-        Object register(Object obj, Runnable action);
-
-        void clean(Object cleanerInstance);
-    }
-
-    private static class JavaRefCleanerWrapper implements CleanerWrapper {
-        private final Object cleaner;
-        private final MethodHandle registerHandle;
-        private final MethodHandle cleanHandle;
-
-        public JavaRefCleanerWrapper() throws Exception {
-
-            Class<?> cleanerClass = Class.forName("java.lang.ref.Cleaner");
-
-            MethodHandle createHandle = MethodHandles.publicLookup()
-                    .findStatic(cleanerClass, "create", MethodType.methodType(cleanerClass));
-            cleaner = createHandle.invoke();
-
-            Class<?> cleanableClass = Class.forName("java.lang.ref.Cleaner$Cleanable");
-            registerHandle = MethodHandles.publicLookup()
-                    .findVirtual(cleanerClass, "register",
-                            MethodType.methodType(cleanableClass, Object.class, Runnable.class));
-
-            cleanHandle = MethodHandles.publicLookup()
-                    .findVirtual(cleanableClass, "clean",
-                            MethodType.methodType(void.class));
+        Runnable action = cleanupActions.remove(reference);
+        if (action != null) {
+            action.run();
         }
-
-        public Object register(Object obj, Runnable action) {
-            try {
-                return registerHandle.invoke(cleaner, obj, action);
-            } catch (Throwable e) {
-                throw new RuntimeException("Failed to register Cleaner", e);
-            }
-        }
-
-        public void clean(Object cleanerInstance) {
-            try {
-                cleanHandle.invoke(cleanerInstance);
-            } catch (Throwable e) {
-                throw new RuntimeException("Failed to clean", e);
-            }
-        }
-    }
-
-    private static class SunMiscCleanerWrapper implements CleanerWrapper {
-        private final MethodHandle createCleanerHandle;
-        private final MethodHandle cleanHandle;
-
-        public SunMiscCleanerWrapper() throws Exception {
-            Class<?> cleanerClass = Class.forName("sun.misc.Cleaner");
-            createCleanerHandle = MethodHandles.publicLookup().findStatic(cleanerClass, "create",
-                    MethodType.methodType(cleanerClass, Object.class, Runnable.class));
-            cleanHandle = MethodHandles.publicLookup().findVirtual(cleanerClass, "clean",
-                    MethodType.methodType(void.class));
-        }
-
-        public Object register(Object obj, Runnable action) {
-            try {
-                return createCleanerHandle.invokeWithArguments(obj, action);
-            } catch (Throwable e) {
-                throw new RuntimeException("Failed to create Cleaner instance", e);
-            }
-        }
-
-        public void clean(Object cleanerInstance) {
-            try {
-                cleanHandle.invokeWithArguments(cleanerInstance);
-            } catch (Throwable e) {
-                throw new RuntimeException("Failed to clean", e);
-            }
-        }
+        reference.clear();
     }
 }
